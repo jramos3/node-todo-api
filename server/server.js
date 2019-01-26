4169d7260c461dc2703585bb4e99f00d50b5a8cc
Jefferson Ramos <ramosjeffersonm@gmail.com>
2019-01-26 23:45:37 +0800

    Added GET /todos/:id route

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { ObjectID } = require("mongodb");
 
 const { mongoose } = require("./db/mongoose");
 const { Todo } = require("./models/todo");
@@ -35,6 +36,27 @@ app.get("/todos", (req, res) => {
       res.status(400).send(e);
     }
   );
+});
+
+//GET /todos/:id
+app.get("/todos/:id", (req, res) => {
+  const { id } = req.params;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then(todo => {
+      if (!todo) {
+        return res.status(404).send();
+      }
+
+      res.send({ todo });
+    })
+    .catch(err => {
+      res.status(400).send();
+    });
 });
 
 app.listen(3000, () => {
