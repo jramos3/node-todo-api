c6578dcf2777b40fe0f7179461fb44914b7a9776
Jefferson Ramos <ramosjeffersonm@gmail.com>
2019-01-26 18:18:41 +0800

    Test POST /todos route

--- server/server.js
+++ server/server.js
@@ -28,6 +28,8 @@ app.listen(3000, () => {
   console.log("Started on port 3000");
 });
 
+module.exports = { app };
+
 // const newTodo = new Todo({ text: "Cook dinner" });
 
 // newTodo.save().then(
