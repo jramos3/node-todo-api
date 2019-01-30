6ea9b2545136d74d7097ae0be9995447f247bc73
Jefferson Ramos <ramosjeffersonm@gmail.com>
2019-01-31 00:08:38 +0800

    Added test cases for DELETE /todos/:id route

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -107,3 +107,42 @@ describe("GET /todos/:id", () => {
       .end(done);
   });
 });
+
+describe("DELETE /todos/:id", () => {
+  it("should remove a todo", done => {
+    const id = todos[0]._id.toHexString();
+
+    request(app)
+      .delete(`/todos/${id}`)
+      .expect(200)
+      .expect(res => {
+        expect(res.body.todo._id).toBe(id);
+      })
+      .end((err, res) => {
+        if (err) {
+          return done(err);
+        }
+
+        Todo.findById(id)
+          .then(todo => {
+            expect(todo).toNotExist();
+            done();
+          })
+          .catch(err => done(err));
+      });
+  });
+
+  it("should return 404 if todo not found", done => {
+    request(app)
+      .delete(`/todos/${new ObjectID().toHexString()}`)
+      .expect(404)
+      .end(done);
+  });
+
+  it("should return 404 if ObjectID is invalid", done => {
+    request(app)
+      .delete("/todos/123")
+      .expect(404)
+      .end(done);
+  });
+});
