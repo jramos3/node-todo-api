76b3d94b904d6b2de85c7aa130b44f28bf5a17ea
Jefferson Ramos <ramosjeffersonm@gmail.com>
2019-01-31 23:21:53 +0800

    Remove comments

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 
-// const url = process.env.PORT ? "mongodb://jeff:test123@ds139534.mlab.com:39534/todoapp" : "mongodb://localhost:27017/TodoApp";
-
 mongoose.Promise = global.Promise;
 mongoose.connect(
   process.env.MONGODB_URI,
