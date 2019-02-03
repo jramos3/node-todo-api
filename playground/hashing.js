a0ad6d0060caf542049303a4828fa023e46a3a86
Jefferson Ramos <ramosjeffersonm@gmail.com>
2019-02-04 01:27:42 +0800

    Added POST users/login route

--- playground/hashing.js
+++ playground/hashing.js
@@ -43,15 +43,19 @@ const bcrypt = require("bcryptjs");
 
 const password = "123abc!";
 
-bcrypt.genSalt(10, (err, salt) => {
-  bcrypt.hash(password, salt, (err, hash) => {
-    console.log(hash);
-  });
-});
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
 const hashedPassword =
   "$2a$10$nGDXQifFp4mwG5YxaF4xq.ftsiw3IJPb6VOUNz3ZLA4Erbh8AiXNO";
 
-bcrypt.compare(password, hashedPassword, (err, res) => {
-  console.log(res);
-});
+// bcrypt.compare(password, hashedPassword, (err, res) => {
+//   console.log(res);
+// });
+
+bcrypt
+  .compare(password, hashedPassword)
+  .then(isMatched => console.log(isMatched));
--- server/server.js
+++ server/server.js
@@ -135,8 +135,24 @@ app.post("/users", (req, res) => {
     .catch(err => res.status(400).send(err));
 });
 
+//GET /users/me
 app.get("/users/me", authenticate, (req, res) => {
   res.send(req.user);
+});
+
+//POST /users/login
+app.post("/users/login", (req, res) => {
+  const body = _.pick(req.body, ["email", "password"]);
+
+  User.findByCredentials(body.email, body.password)
+    .then(user => {
+      user.generateAuthToken().then(token => {
+        res.header("x-auth", token).send(user);
+      });
+    })
+    .catch(err => {
+      res.status(400).send(err);
+    });
 });
 
 app.listen(port, () => {
