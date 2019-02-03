906bf53a22681a9556038dc5b2c3038101ff03fc
Jefferson Ramos <ramosjeffersonm@gmail.com>
2019-02-03 13:36:55 +0800

    Hashed user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -27,14 +27,31 @@
 //   console.log("Data was changed. Do not trust!");
 // }
 
-const jwt = require("jsonwebtoken");
+// const jwt = require("jsonwebtoken");
 
-const data = {
-  id: 10
-};
+// const data = {
+//   id: 10
+// };
 
-const token = jwt.sign(data, "123abc");
-console.log(token);
+// const token = jwt.sign(data, "123abc");
+// console.log(token);
 
-const decoded = jwt.verify(token, "123abc");
-console.log(decoded);
+// const decoded = jwt.verify(token, "123abc");
+// console.log(decoded);
+
+const bcrypt = require("bcryptjs");
+
+const password = "123abc!";
+
+bcrypt.genSalt(10, (err, salt) => {
+  bcrypt.hash(password, salt, (err, hash) => {
+    console.log(hash);
+  });
+});
+
+const hashedPassword =
+  "$2a$10$nGDXQifFp4mwG5YxaF4xq.ftsiw3IJPb6VOUNz3ZLA4Erbh8AiXNO";
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
